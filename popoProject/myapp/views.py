232f6775ddfa262769138224e72d78788f41778f
unknown <poorya2293@gmail.com>
2023-05-04 23:40:22 +0430

    introduction to django models beginer

--- popoProject/myapp/views.py
+++ popoProject/myapp/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Feature
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our service is very quick'
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'hight'
+    feature2.details = 'see futear'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'ultra'
+    feature3.details = 'over the level'
+
+    return render(request, 'index.html', {'feature1': feature1, 'feature2': feature2, 'feature3': feature3})
 
 
 def counter(request):
