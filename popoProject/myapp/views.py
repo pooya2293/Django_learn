7960ed89a33876e1462a03dba3c4dce191d46e31
unknown <poorya2293@gmail.com>
2023-05-10 00:10:15 +0430

    user login and logout

--- popoProject/myapp/views.py
+++ popoProject/myapp/views.py
@@ -38,6 +38,28 @@ def register(request):
         return render(request, 'register.html')
 
 
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'دیتای نامعتبر')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
 def counter(request):
     # we experinced just ok with text name in index and here
     text = request.POST['text']
